725167db6c5ee2691f98b04db04c33314d327e63
artd <artd@163.com>
2022-08-02 18:28:47 +0800

    feat: 支持自定义package.json

--- bin/artd.js
+++ bin/artd.js
@@ -4,10 +4,19 @@
 const inquirer = require('inquirer');
 const log = require('../lib/log');
 const init = require('../lib/console/init');
+const githubRequest = require('../lib/handleRequest');
 const program = require('commander');
 const pkg = require('../package.json');
+const ora = require('ora');
+const chalk = require('chalk');
+const utils = require('../lib/utils');
 
+const spinner = ora('Downloading template...');
 const temps = ['vue-admin', 'antd-admin'];
+
+program
+    .option('-v, --version', 'output the version')
+    .description('art commander');
 
 program
     .usage('<command> [template]')
@@ -18,27 +27,43 @@ program.on('--help', function () {
     help();
 });
 
-let config = [
+const TEMPLATES = [
     {
-        type: 'checkbox',
-        message: '请选择',
-        name: 'select',
-        choices: [
-            new inquirer.Separator(' = 前台程序 = '),
-            {
-                name: 'vue-admin',
-                value: 'vue-admin'
-            },
-            {
-                name: 'antd-admin',
-                value: 'antd-admin'
+        value: 'vue-admin',
+        name: 'vue-admin',
+        des: 'vue2 + element-ui + axios',
+        template: 'vue-admin'
+    },
+    {
+        value: 'antd-admin',
+        name: 'antd-admin',
+        des: 'react + antd',
+        template: 'antd-admin'
+    }
+];
+
+let defaultConfig = [
+    {name: 'name', message: '请输入项目名', default: 'project-name'},
+    {name: 'description', message: '描述', default: '项目描述'},
+    {name: 'version', message: '版本号?', default: '0.0.1'},
+    {name: 'author', message: '作者?', default: utils.getGitInfo('user.name')},
+    {name: 'template', message: '请选择模板类型?', type: 'list', choices: TEMPLATES.map(item => {
+        let {name, value} = item;
+        return {name, value};
+    }), default: 'vue-admin'},
+    {
+        name: 'confirm',
+        message: '是否确认生成项目',
+        default: 'Yes'
+    },
+    {
+        name: 'judege',
+        message: '',
+        default: '',
+        when: function(anwser) {
+            if (['y', 'Y', 'Yes', 'yes', 'YES'].indexOf(anwser.confirm) < 0) {
+                process.exit();
             }
-        ],
-        validate: function (answer) {
-            if (answer.length !== 1) {
-                return '只能选择一个初始化项目模板';
-            }
-            return true;
         }
     }
 ];
@@ -64,31 +89,50 @@ function runCommand(commands) {
         if(secondCommand) {
             if(temps.includes(secondCommand)) {
                 log.info(`正在开始为您初始化项目(${secondCommand}).......`);
-                let template = secondCommand;
-                let dirPath = secondCommand;
-                init({
-                    template,
-                    dirPath
-                });
+                spinner.start();
+                githubRequest.handleRequest()
+                    .then(() => {
+                        spinner.succeed(chalk.green('Get template form remote successfully'));
+                        let template = secondCommand;
+                        const config = defaultConfig.filter(item => item.name !== 'template');
+                        initFunction(config, template);
+                    }).catch(() => {
+                        spinner.warn('Get template fail');
+                    });
             }else {
                 program.help(); 
             }
         }else {
-            inquirer.prompt(config)
-                .then(data => {
-                    let template = data.select[0];
-                    let dirPath = data.select[0];
-                    log.info(`项目选择成功，正在开始为您初始化项目(${template}).......`);
-                    init({
-                        template,
-                        dirPath
-                    });
+            log.info('Get template form remote ......');
+            spinner.start();
+            githubRequest.handleRequest()
+                .then(() => {
+                    spinner.succeed(chalk.green('Get template form remote successfully'));
+                    initFunction();
+                })
+                .catch(() => {
+                    spinner.warn('Get template fail');
                 });
         }
         break;
     default:
         program.help();
     } 
+}
+
+function initFunction (config = defaultConfig, tpl) {
+    inquirer.prompt(config)
+        .then(data => {
+            const config = data;
+            const template = tpl || config.template;
+            const dirPath = config.name;
+            log.info(`项目选择成功，正在开始为您初始化项目(${template}).......`);
+            init({
+                template,
+                dirPath,
+                config
+            });
+        });
 }
 
 // 自定义 help
